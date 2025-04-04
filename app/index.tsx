31a49bc2d48f00e595b86aa218345c1a21dda351
wdotgonzales <wdotgonzales@gmail.com>
2025-04-04 17:07:19 +0800

    ah basta

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
 import { Text, View } from "react-native";
+import { Image } from "react-native";
+import { StyleSheet } from "react-native";
+import TrackJobLogo from '../assets/images/trackjob-logo.svg';
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.mainContainer}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TrackJobLogo width={120} height={40} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  trackJobLogo: {
+
+  }
+});
