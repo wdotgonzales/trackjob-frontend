c3d056e84c74c7f84df292b324a7dd35b9839e4b
wdotgonzales <wdotgonzales@gmail.com>
2025-04-05 13:04:09 +0800

    create introduction page

---
+++ app/components/IntroductionPage.tsx
@@ -0,0 +1,63 @@
+import { Text, View, Pressable } from "react-native";
+import { StyleSheet } from "react-native";
+import TrackJobLogo from "../../assets/images/trackjob-logo.svg";
+
+export default function IntroductionPage() {
+  return (
+    <View style={styles.mainContainer}>
+      <TrackJobLogo width={270} style={styles.trackJobLogo} />
+      <Text style={styles.trackJobText}>
+        <Text style={{ color: "#06748F" }}>Track</Text>
+        <Text style={{ color: "#F97009" }}>Job</Text>
+      </Text>
+      <Text style={styles.trackJobDescription}>
+        Keep track of your hiring journey
+      </Text>
+      <Pressable>
+        <View style={styles.startButtonContainer}>
+          <Text style={styles.startButtonText}>Start</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#054B63",
+  },
+
+  trackJobLogo: {
+    position: "relative",
+    right: 14,
+  },
+
+  trackJobText: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+
+  trackJobDescription: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "300",
+    marginTop: 10,
+  },
+
+  startButtonContainer: {
+    marginTop: 25,
+    backgroundColor: "#052731",
+    width: 180,
+    borderRadius: 20,
+  },
+
+  startButtonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 18,
+    paddingBlock: 9,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,4 @@
-import { Text, View } from "react-native";
-import { Image } from "react-native";
-import { StyleSheet } from "react-native";
-import TrackJobLogo from '../assets/images/trackjob-logo.svg';
-
+import IntroductionPage from "./components/IntroductionPage";
 export default function Index() {
-  return (
-    <View
-      style={styles.mainContainer}
-    >
-      <TrackJobLogo width={120} height={40} />
-    </View>
-  );
+  return <IntroductionPage />;
 }
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  trackJobLogo: {
-
-  }
-});
