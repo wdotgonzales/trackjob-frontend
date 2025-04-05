72f10f2b361b95421f5432044ace4ae76476fc98
wdotgonzales <wdotgonzales@gmail.com>
2025-04-05 16:44:21 +0800

    transfer files to screensdirectory

---
+++ app/screens/Guidelines.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, Pressable } from "react-native";
+import { StyleSheet } from "react-native";
+import { Router, router } from "expo-router";
+
+export default function Guidelines() {
+  return <View style={styles.mainContainer}></View>;
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {},
+});
