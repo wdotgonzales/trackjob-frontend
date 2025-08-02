c780f9b0f5033d4d4041a4a724921cc7886b664d
wdotgonzales <wdotgonzales@gmail.com>
2025-08-02 16:38:06 +0800

    feat: setup reducers for store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authentication/authSlice"
+import loaderReducer from "../features/loader/loaderSlice"
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: authReducer,
+        loader: loaderReducer
+    }
 })
 
