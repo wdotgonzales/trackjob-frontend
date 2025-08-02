ab1b75d521abe356fdf900cf844351375fa2729f
wdotgonzales <wdotgonzales@gmail.com>
2025-08-02 16:40:33 +0800

    feat: implement authenticated navigators

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,26 +4,48 @@ import StartScreen from "../screens/StartScreen"
 import FeatureScreen from "../screens/FeaturesScreen"
 import LoginScreen from "../screens/LoginScreen"
 
+import { useSelector } from "react-redux";
+import AuthenticatedTabNavigator from "./AuthenticatedTabNavigator"
+import CustomLoader from "../components/CustomLoader"
+
 const Stack = createNativeStackNavigator()
 
 const AppNavigator = () => {
+
+    // This is only temporary, for now this will dictate if user is authenticated or not.
+    const auth = useSelector((state) => state.auth.isAuthenticated);
+
+    const isVisible = useSelector((state) => state.loader.isVisible);
+
     return (
-    <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen 
-                name="Start" 
-                component={StartScreen} 
+        <>
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{ headerShown: false }}>
+                    {auth ? (
+                        // If user is authenticated, show the tab navigator
+                        <Stack.Screen 
+                            name="AuthenticatedApp" 
+                            component={AuthenticatedTabNavigator} 
+                        />
+                    ) : (
+                        // If user is not authenticated, show the authentication flow
+                        <>
+                            <Stack.Screen name="Start" component={StartScreen} />
+                            <Stack.Screen name="Features" component={FeatureScreen} />
+                            <Stack.Screen name="Login" component={LoginScreen}/>
+                        </>
+                    )}
+                </Stack.Navigator>
+            </NavigationContainer>
+
+
+            <CustomLoader 
+                visible={isVisible}
+                size={80}
+                color="#F97009"
+                backgroundColor="rgba(255, 255, 255, 0.3)"
             />
-            <Stack.Screen
-                name="Features"
-                component={FeatureScreen}
-            />
-            <Stack.Screen
-                name="Login"
-                component={LoginScreen}
-            />
-        </Stack.Navigator>
-    </NavigationContainer>
+        </>
     )
 }
 
