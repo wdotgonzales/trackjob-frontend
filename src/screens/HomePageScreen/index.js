f201c2ccef45ac916ad4d13ae3c5f41409af6dfc
wdotgonzales <wdotgonzales@gmail.com>
2025-08-02 09:18:14 +0800

    feat: implement home page screen

---
+++ src/screens/HomePageScreen/index.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+    return <View>
+        <Text>Hi from FeatureScreen</Text>
+    </View>
+}
+
+export default HomeScreen
