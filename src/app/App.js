4632cbaa3a45aae47d80872ed6f5a21a1d654a74
wdotgonzales <wdotgonzales@gmail.com>
2025-08-02 12:53:23 +0800

    feature: add toast component

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,15 @@
 import AppNavigator from "../navigation/AppNavigator";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import Toast from "react-native-toast-message";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <AppNavigator/>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <AppNavigator/>
+      </Provider>
+      <Toast />
+    </>
   );
 }
