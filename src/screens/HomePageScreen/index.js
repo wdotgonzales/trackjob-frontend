e46de0e064059ceb7d6bace58a9b0e364b7f0b73
wdotgonzales <wdotgonzales@gmail.com>
2025-08-04 13:55:10 +0800

    refactor: setup template for future implementation

--- src/screens/HomePageScreen/index.js
+++ src/screens/HomePageScreen/index.js
@@ -1,23 +1,36 @@
 import { View, Text, StyleSheet } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { useState, useEffect } from "react"
+import { useFocusEffect } from "@react-navigation/native"
+import { useCallback } from "react"
 
 const HomePageScreen = () => {
-    return (
-        <View style={styles.mainContainer}>
-            <SafeAreaView>
-                <View>
-                    <Text>HomePageScreen Screen</Text>
-                </View>
-            </SafeAreaView>
+  const [test, useTest] = useState(true)  
+
+  useFocusEffect(
+    useCallback(() => {
+        if(!test){
+            console.log("home screen is focused/visible")
+        }
+    }, [])
+  )
+
+  return (
+    <View style={styles.mainContainer}>
+      <SafeAreaView>
+        <View>
+          <Text>HomePageScreen Screen</Text>
         </View>
-    )
+      </SafeAreaView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        backgroundColor: "#054B63"
-    }
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#054B63"
+  }
 })
 
 export default HomePageScreen
--- src/screens/SettingsScreen/index.js
+++ src/screens/SettingsScreen/index.js
@@ -1,14 +1,19 @@
 import { View, Text, StyleSheet } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { useDispatch } from "react-redux"
+import { logout } from "../../features/authentication/authSlice"
 
 const SettingsScreen = () => {
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView>
             <View>
                 <Text>SettingsScreen Screen</Text>
+                <Text onPress={() => dispatch(logout())}>Logout</Text>
             </View>
         </SafeAreaView>
     )
 }
 
-export default SettingsScreen
+export default SettingsScreen;
