10317325d916764af48c04f38dc6297dfd51fb72
wdotgonzales <wdotgonzales@gmail.com>
2025-08-04 13:57:09 +0800

    refactor: remove loader reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authentication/authSlice"
-import loaderReducer from "../features/loader/loaderSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        loader: loaderReducer
     }
 })
 
