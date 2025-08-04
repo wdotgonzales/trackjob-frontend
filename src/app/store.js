dddc0628920d81914c739c77888d756081df961a
wdotgonzales <wdotgonzales@gmail.com>
2025-08-05 07:51:33 +0800

    feat: add profile reducer into store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authentication/authSlice"
+import authReducer from "../features/authentication/authSlice";
+import userProfileReducer from "../features/profile/profileSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        profile: userProfileReducer,
     }
 })
 
