5302e067f1450eaaa629b0a055c530601719a033
wdotgonzales <wdotgonzales@gmail.com>
2025-08-05 15:03:34 +0800

    feat: add Google Sign-In configuration with iOS and web client IDs

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,17 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import Toast from "react-native-toast-message";
 
+import { useEffect } from "react";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
 export default function App() {
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId: "688288835541-78ourmcu09tcknofiio36rt3qrq4mfoh.apps.googleusercontent.com",
+      webClientId: "688288835541-mtrkjup4rv0cfd4oambdf1nuc1ilns7r.apps.googleusercontent.com",
+      profileImageSize: 150,
+    })
+  }, [])
   return (
     <>
       <Provider store={store}>
