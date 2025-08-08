159ea2f8710227af3a870817d2eacce16f9cd546
wdotgonzales <wdotgonzales@gmail.com>
2025-08-09 07:12:41 +0800

    feat: Added registration-related screens: Register, Otp, ChooseProfilePicture, and RegisterSuccess

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import StartScreen from "../screens/StartScreen"
 import FeatureScreen from "../screens/FeaturesScreen"
 import LoginScreen from "../screens/LoginScreen"
+import RegisterScreen from "../screens/RegisterScreen"
+import ChooseProfilePictureScreen from "../screens/RegisterScreen/screens/ChooseProfilePictureScreen"
+import OtpScreen from "../screens/RegisterScreen/screens/OtpScreen"
+import RegisterSuccessScreen from "../screens/RegisterScreen/screens/RegisterSuccessScreen"
 import { useSelector } from "react-redux";
 import AuthenticatedTabNavigator from "./AuthenticatedTabNavigator"
 import { useEffect } from "react";
@@ -35,6 +39,10 @@ const AppNavigator = () => {
                             <Stack.Screen name="Start" component={StartScreen} />
                             <Stack.Screen name="Features" component={FeatureScreen} />
                             <Stack.Screen name="Login" component={LoginScreen}/>
+                            <Stack.Screen name="Register" component={RegisterScreen}/>
+                            <Stack.Screen name="Otp" component={OtpScreen} />
+                            <Stack.Screen name="RegisterProfilePicture" component={ChooseProfilePictureScreen}/>
+                            <Stack.Screen name="RegisterSuccess" component={RegisterSuccessScreen}/>
                         </>
                     )}
                 </Stack.Navigator>
