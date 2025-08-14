e97ff5214c8ed63a5d897ede0fd866795c5b8740
wdotgonzales <wdotgonzales@gmail.com>
2025-08-14 12:47:14 +0800

    feat: add jobApplication reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authentication/authSlice";
 import userProfileReducer from "../features/profile/profileSlice";
+import jobApplicationReducer from "../features/jobApplication/jobApplication";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         profile: userProfileReducer,
+        jobApplication: jobApplicationReducer,
     }
 })
 
