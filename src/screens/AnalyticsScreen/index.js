a21adb055d44907a8fdcd8cf1527179dfd017328
wdotgonzales <wdotgonzales@gmail.com>
2025-08-19 15:05:04 +0800

    feat: dashboard

--- src/screens/AnalyticsScreen/index.js
+++ src/screens/AnalyticsScreen/index.js
@@ -1,14 +1,28 @@
-import { View, Text, StyleSheet } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { View, Text, StyleSheet, Image } from "react-native"
 
 const AnalyticsScreen = () => {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Analytics Screen</Text>
-            </View>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <Image source={require("../../../assets/dashboard.png")} style={{ height: 200, width: 300, }}/>
+            <Text style={styles.text}>Coming Soon...</Text>
+        </View>
     )
 }
 
-export default AnalyticsScreen
+export default AnalyticsScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#054B63",
+        justifyContent: "center",  // vertical center
+        alignItems: "center"       // horizontal center
+    },
+
+    text: {
+        fontSize: 18,
+        color: "white",
+        marginTop: 18,
+        fontWeight: "bold"
+    }
+})
